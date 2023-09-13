309b03e56052b65235d29860f819084ba21b85fa
Zibi Braniecki <zibi@amazon.com>
2023-09-12 20:25:52 -0700

    Introduce mui tabs

---
+++ src/app/[[...id]]/page.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from "next/link";
+import { usePathname } from 'next/navigation';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const tabs = [
+  "/foo",
+  "/bar",
+  "/baz",
+];
+
+function getTabIdx(tabs, path) {
+  const idx = tabs.indexOf(path);
+  if (idx === -1) {
+    return 0;
+  }
+  return idx;
+}
+
+export default function BasicTabs() {
+  const path = usePathname();
+  const tabIdx = getTabIdx(tabs, path);
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={tabIdx} aria-label="basic tabs example">
+          <Tab label="Item One" {...a11yProps(0)} component={Link} href="/foo" />
+          <Tab label="Item Two" {...a11yProps(1)} component={Link} href="/bar" />
+          <Tab label="Item Three" {...a11yProps(2)} component={Link} href="/baz" />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={tabIdx} index={0}>
+        Item One
+      </CustomTabPanel>
+      <CustomTabPanel value={tabIdx} index={1}>
+        Item Two
+      </CustomTabPanel>
+      <CustomTabPanel value={tabIdx} index={2}>
+        Item Three
+      </CustomTabPanel>
+    </Box>
+  );
+}
